33b462c3e66cc239a51177af6d621a938165d231
unknown <mhmd.abbass379@gmail.com>
2025-02-20 21:19:09 +0200

    Started with the script
    Added salary and month inputs and finances list

---
+++ nabiha-salary.py
@@ -0,0 +1,7 @@
+print ("Hello Nabiha\n")
+
+salary = int(input("Please enter your Salary:\n"))
+
+month = input("Please enter the month of this salaey:\n")
+
+finances = ["Savings", "Rent", "Electricity"]
