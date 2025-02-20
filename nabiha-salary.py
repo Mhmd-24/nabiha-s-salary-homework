2c838b5eaa181e1c3ddf00cc9931cf8bca21dff1
unknown <mhmd.abbass379@gmail.com>
2025-02-20 22:00:58 +0200

    Added percentages calculating, total and remainder amounts
    Manifested for loops for the percentages and stored them in a list and other for loop to
    output each amount to each finance and calculated the total and the remainder

--- nabiha-salary.py
+++ nabiha-salary.py
@@ -2,6 +2,25 @@ print ("Hello Nabiha\n")
 
 salary = int(input("Please enter your Salary:\n"))
 
-month = input("Please enter the month of this salaey:\n")
+month = input("Please enter the month of this salary:\n")
 
 finances = ["Savings", "Rent", "Electricity"]
+
+percentages = []
+
+for i in range(len(finances)):
+    percentage = int(input("Enter the percentage for " + finances[i] +"\n"))
+
+    percentages.append(percentage)
+
+for i in range(len(percentages)):
+    result = percentages[i] * salary / 100
+    print("The amount allocated to " + str(finances[i]) + " is: " + str(result) + " $\n")
+
+total = sum(percentages) * salary / 100
+
+print("The total spending is: " + str(total) + " $\n")
+
+remainder = salary - total
+
+print("The remainder is: " + str(remainder) + " $\n")
